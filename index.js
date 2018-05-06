f2a0b093dc34fad5e22c9c71a56fd4217f879a8e
WanchaloemMacBookPro <wanchaloemmacbookpro@WanchaloemMacBookPros-MacBook-Pro.local>
2018-05-06 14:19:02 +0700

    Tranning 1-3 - นำตัวอย่าง Fontend สำหรับเร็จรูปที่จะทำการเชื่อมต่อมา

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use('/', express.static(__dirname + '/client'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/client/index.html'));
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
